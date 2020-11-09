f51a06745b25897e36eef6d62a9985726654985d
Erwin Heldy G <erwin.developer@gmail.com>
2020-11-09 10:52:00 +0700

    Change unit to em, add touch support

--- star-rating.js
+++ star-rating.js
@@ -8,28 +8,33 @@ const starRating = (element, config = {}) => {
   const stars = 5
   element.setAttribute('hidden', true)
   element.value = element.value > stars ? stars : element.value
-  const size = config.size || element.dataset.starRatingSize || 24
+  const size = config.size || element.dataset.starRatingSize || 1.5
   const width = size * stars
   const initValue = (100 / stars) * element.value
   element.insertAdjacentHTML('beforebegin', `<div class="star-rating"><div class="star-rating-value" style="width: ${initValue}%" data-star-rating-width="${initValue}%"></div></div>`)
   const container = element.previousSibling
   const children = container.getElementsByClassName('star-rating-value')[0]
 
-  container.style.width = width + 'px'
-  container.style.height = size + 'px'
-  container.style.backgroundSize = size + 'px'
-  children.style.backgroundSize = size + 'px'
+  container.style.width = width + 'em'
+  container.style.height = size + 'em'
+  container.style.backgroundSize = size + 'em'
+  children.style.backgroundSize = size + 'em'
   element.readOnly && (container.style.cursor = 'auto')
 
   if (element.readOnly === false) {
     const getPercent = e => {
       const rect = e.target.getBoundingClientRect()
-      const x = e.clientX - rect.left
+      let clientX = e.clientX
+      if (clientX === undefined) {
+        clientX = e.touches.length ? e.touches[0].clientX : e.changedTouches[0].clientX
+      }
+      const x = clientX - rect.left
       const percent = x / container.offsetWidth * 100
       return Math.ceil(Math.ceil(percent) / 10) * 10 + '%'
     }
     const setValue = () => {
-      const newValue = children.dataset.starRatingWidth.replace('%', '') / (100 / stars)
+      let newValue = children.dataset.starRatingWidth.replace('%', '') / (100 / stars)
+      newValue = newValue < 0 ? 0 : (newValue > stars ? stars : newValue)
       if (element.value != newValue) {
         element.value = newValue
         element.setAttribute('value', newValue)
@@ -37,8 +42,13 @@ const starRating = (element, config = {}) => {
       }
     }
     container.addEventListener('mousemove', e => children.style.width = getPercent(e))
+    container.addEventListener('touchmove', e => children.style.width = getPercent(e))
     container.addEventListener('mouseleave', () => children.style.width = children.dataset.starRatingWidth)
     container.addEventListener('click', e => {
+      children.dataset.starRatingWidth = getPercent(e)
+      setValue()
+    })
+    container.addEventListener('touchend', e => {
       children.dataset.starRatingWidth = getPercent(e)
       setValue()
     })
